f10efcd810207c9d0dff282c1bdd8d7dabf6f347
estebanfern <estebangfernandeza@gmail.com>
2022-12-15 09:34:04 -0300

    Correccion de bugs

--- src/main/java/org/calculadora/Calculadora.java
+++ src/main/java/org/calculadora/Calculadora.java
@@ -12,7 +12,7 @@ package org.calculadora;
 public class Calculadora {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Running");
         App.main(args);
     }
 }
