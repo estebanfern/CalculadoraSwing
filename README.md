bc1d4e0eb9ba6f91b33d1e82c5f6326d6866597b
estebanfern <estebangfernandeza@gmail.com>
2022-12-14 21:17:58 -0300

    Proyecto terminado

---
+++ README.md
@@ -0,0 +1,1 @@
+Calculadora hecha en Java con Swing inspirada en la estandar de Windows 11.
---
+++ src/main/java/org/calculadora/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package org.calculadora;
+
+/**
+ *
+ * @author esteb
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        App.main(args);
+    }
+}
